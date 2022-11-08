4dd24204b8c947dd462eea1b632a4944181531e0
Adi <37550188+adikulkarni11@users.noreply.github.com>
2022-11-08 14:56:22 -0500

    Fixed working checksum permutation code

--- agents/agent7.py
+++ agents/agent7.py
@@ -67,11 +67,61 @@ class EncoderDecoder:
         return perm
 
     def str_to_perm(self, s):
-        ret = ''
+        '''ret = ''
+        print("S:",s)               # TODO: change max_chars to a global variable
         for c in s[:14]:
             ret += self.char_dict[c]
         n = int(ret, 2)
-        return self.nth_perm(n)
+        return self.nth_perm(n)'''
+        max_chars = 12
+        if len(s) > max_chars:
+            message = s[:max_chars]
+
+        num = self.str_to_num(s)
+        perm = self.num_to_perm(num)
+        return perm
+
+    def perm_to_str(self, permutation):
+        num = self.perm_to_num(permutation)
+        words = []                      # TODO: use binary to make faster?
+        break_next = False
+        while True:
+            index = num % len(self.chars)
+            words.append(self.chars[index])
+            num = num // len(self.chars)
+            if break_next:
+                break
+            if num == 0:
+                break_next = True
+        return ''.join(words[::-1]).strip()
+
+        '''n = self.perm_number(permutation)
+        print('en', n)
+        binary_string = self.to_binary(n)
+        print(binary_string)
+        binary_string = '0' * ((6 - len(binary_string) % 6) % 6) + binary_string
+        print(binary_string)
+        ret = ''
+        print(self.bin_dict)
+        for i in range(0, len(binary_string) - 5, 6):
+            print(binary_string[i:i+6])
+            if binary_string[i:i + 6] not in self.bin_dict:
+                return 'PARTIAL: '
+            ret += self.bin_dict[binary_string[i:i + 6]]
+        return ret'''
+
+    def perm_to_num(self, permutation):
+        # Adapted from Group 1
+        n = len(permutation)
+        number = 0
+        for i in range(n):
+            k = 0
+            for j in range(i + 1, n):
+                if permutation[j] < permutation[i]:
+                    k += 1
+            number += k * self.factorials[i]
+
+        return number
 
     def str_to_num(self, message):
         # Adapted from Group 1 Agent
@@ -84,6 +134,8 @@ class EncoderDecoder:
                 break
         while len(tokens) < self.max_messge_length:
             tokens.append(" ")
+
+        print(tokens)
         tokens = tokens[::-1]
 
         # Convert char to int
@@ -93,8 +145,6 @@ class EncoderDecoder:
             num = 0
             for idx, ch in enumerate(tokens):   #TODO utilize char_dict, etc
                 num += self.chars.index(ch) * len(self.chars) ** idx
-
-            # Check if message can fit in N cards
             if num // self.factorials[0] < len(self.perm_zero):
                 break
             else:
@@ -110,31 +160,6 @@ class EncoderDecoder:
             perm.append(items.pop(lehmer))
             n %= f
         return perm
-
-    def perm_to_str(self, permutation):
-        n = self.perm_number(permutation)
-        binary_string = self.to_binary(n)
-        binary_string = '0' * ((6 - len(binary_string) % 6) % 6) + binary_string
-
-        ret = ''
-        for i in range(0, len(binary_string) - 5, 6):
-            if binary_string[i:i + 6] not in self.bin_dict:
-                return 'PARTIAL: '
-            ret += self.bin_dict[binary_string[i:i + 6]]
-        return ret
-
-    def perm_to_num(self, permutation):
-        # Adapted from Group 1
-        n = len(permutation)
-        number = 0
-        for i in range(n):
-            k = 0
-            for j in range(i + 1, n):
-                if permutation[j] < permutation[i]:
-                    k += 1
-            number += k * self.factorials[i]
-        return number
-
 
     def set_checksum(self, num, base=10):
         num_bin = bin(num)[2:]
@@ -166,15 +191,11 @@ class Agent:
         self.perm_ck = EncoderDecoder(6)
 
     def encode(self, message):
-        print('Encoding...')
-        print('message to encode: ', message)
+        #print('Encoding "', message, '"')
 
         x  = self.ed.str_to_num(message)
         checksum = self.ed.set_checksum(x)
-        print('Checksum int_val w/ set_checksum:', checksum)
         checksum_cards = self.perm_ck.num_to_perm(checksum)
-        print('Checksum Cards: ', checksum_cards)
-
         encoded_deck = list(range(48 - self.encoding_len - 2)) + self.ed.str_to_perm(message) + checksum_cards
         
         print('Encoded deck:\n', encoded_deck, '\n---------')
@@ -183,8 +204,6 @@ class Agent:
     def decode(self, deck):
 
         print('\n')
-        print('Decoding...')
-        print('Deck to decode:', deck)
         msg_perm = []
         checksum = []
         for card in deck:
@@ -193,165 +212,15 @@ class Agent:
             if card > 45:
                 checksum.append(card)
 
-        print('\nMessage Cards:', msg_perm)
-        print('Checksum Cards:', checksum)
+        #print('\nMessage Cards:', msg_perm)
+        #print('Checksum Cards:', checksum)
+        msg_num = self.ed.perm_to_num(msg_perm)
 
         decoded_checksum = self.perm_ck.perm_to_num(checksum)
-        print('Checksum int_val using perm_to_num: ', decoded_checksum)
-
-        ### We get same int_val as encode using perm_to_num of checksum cards. Now need to get checksum val from message cards itself
-        
-        msg_num = self.ed.perm_to_num(msg_perm)
-        message_checksum = self.ed.set_checksum(msg_num)
-        print(message_checksum) #GETTING WRONG VALUE HERE
-
-        print('decoded_checksum', decoded_checksum)
-        print('message_Checksum', message_checksum)
-        
+        message_checksum = self.perm_ck.set_checksum(msg_num)        
 
         if message_checksum != decoded_checksum:
-            print("MESSAGE CHECKSUM IS NOT EQUAL TO DECODED CHECKSUM")
+            #print("MESSAGE CHECKSUM IS NOT EQUAL TO DECODED CHECKSUM")
             return "NULL"
         else:
-            print(self.ed.perm_to_str(msg_perm))
             return self.ed.perm_to_str(msg_perm)
-
-'''
-import math
-import hashlib
-
-class EncoderDecoder:
-    def __init__(self, n=26):
-        self.encoding_len = n
-        characters = " 1234567890abcdefghijklmnopqrstuvwxyz.," 
-        self.char_dict, self.bin_dict = self.binary_encoding_dicts(characters)
-        self.perm_zero = list(range(48-n, 48)) #[24,25,...48,49]
-
-        factorials = [0] * n
-        for i in range(n):
-            factorials[i] = math.factorial(n-i-1)
-        self.factorials = factorials
-
-    @staticmethod
-    def to_binary_padded(n):
-        ret = ''
-        while n > 1:
-            ret += str(n % 2)
-            n = n // 2
-        ret += str(n)
-        return '0' * (6 - len(ret)) + ret[::-1]
-
-    @staticmethod
-    def to_binary(n):
-        ret = ''
-        while n > 1:
-            ret += str(n % 2)
-            n = n // 2
-        ret += str(n)
-        return ret[::-1]
-
-    def binary_encoding_dicts(self, characters):
-        char_dict = {}
-        bin_dict = {}
-        for i in range(len(characters)):
-            b = self.to_binary_padded(i)
-            char_dict[characters[i]] = b
-            bin_dict[b] = characters[i]
-        return char_dict, bin_dict
-
-    def perm_number(self, permutation):
-        n = len(permutation)
-        s = sorted(permutation)
-        number = 0
-
-        for i in range(n):
-            k = 0
-            for j in range(i + 1, n):
-                if permutation[j] < permutation[i]:
-                    k += 1
-            number += k * self.factorials[i]
-        return number
-
-    def nth_perm(self, n):
-        perm = []
-        items = self.perm_zero[:]
-        for f in self.factorials:
-            lehmer = n // f
-            perm.append(items.pop(lehmer))
-            n %= f
-        return perm
-
-    def str_to_perm(self, s):
-        ret = ''
-        for c in s[:14]:
-            ret += self.char_dict[c]
-        n = int(ret, 2)
-        return self.nth_perm(n)
-
-    def str_to_bitstr(self, s):
-        ret = ''
-        for c in s[:14]:
-            ret += self.char_dict[c]
-        n = int(ret, 2)
-        return n
-
-    def perm_to_str(self, permutation):
-        n = self.perm_number(permutation)
-        binary_string = self.to_binary(n)
-        binary_string = '0' * ((6 - len(binary_string) % 6) % 6) + binary_string
-
-        ret = ''
-        for i in range(0, len(binary_string) - 5, 6):
-            if binary_string[i:i + 6] not in self.bin_dict:
-                return 'PARTIAL: '
-            ret += self.bin_dict[binary_string[i:i + 6]]
-        return ret
-
-    def set_checksum(self, bits: str, checksum_bits=10) -> str:
-        bitstr = bytes(bits, "utf-8")
-        hasher = hashlib.blake2b(bitstr).hexdigest()
-        scale,num_bits = 16,8
-        binstr = bin(int(hasher, scale))[2:].zfill(num_bits)
-        return binstr[-checksum_bits:]
-
-
-    def get_checksum(self, perm):
-        pass
-
-    def to_bin_string(self, value: int) -> str:
-        
-        return bin(value)[2:]
-
-class Agent:
-    def __init__(self, encoding_len=26):
-        self.encoding_len = encoding_len
-        self.ed = EncoderDecoder(self.encoding_len)
-
-    def encode(self, message):
-        x  = self.ed.str_to_bitstr(message)
-        print('xxx',x, type(x))
-        y = self.ed.to_bin_string(x)
-        print(y, type(y))
-        checksum = self.ed.set_checksum(y)
-        print('ccc', checksum, type(checksum))
-        yerr = self.ed.str_to_perm(checksum)
-        print(yerr, type(yerr))
-
-        sds
-        return list(range(48 - self.encoding_len)) + self.ed.str_to_perm(message) + checksum
-
-
-    def decode(self, deck):
-        perm = []
-        for card in deck:
-            if 24 <= card <= 48:
-                perm.append(card)
-        print(perm)
-        if perm[-4:] != self.ed.get_checksum(deck): #deck or perm
-            return "NULL"
-
-        return self.ed.perm_to_str(perm[:-2])
-
-
-
-'''
